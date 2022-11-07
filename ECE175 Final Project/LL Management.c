6104015e35caab70f03b64afbc5562a8f55d4d02
Graham-Nelles <116527372+Graham-Nelles@users.noreply.github.com>
2022-11-06 23:20:47 -0700

    Finished initial version of all functions in LL Management

--- ECE175 Final Project/LL Management.c
+++ ECE175 Final Project/LL Management.c
@@ -11,3 +11,23 @@ typedef struct card_s
 	struct card_s* next;
 } card;
 
+card node_create(char suit, int face)
+{
+	card new;
+	new.suit = suit;
+	new.face = face;
+	return new;
+}
+
+void node_append(card* node_end, card* node_new)
+{
+	node_end->next = node_new;
+	return;
+}
+
+void node_insert(card* node_before, card* node_new, card* node_after)
+{
+	node_before->next = node_new;
+	node_new->next = node_after;
+	return;
+}
